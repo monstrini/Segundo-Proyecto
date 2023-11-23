577506943dddcd5ab4059d157fbb9b527fdab178
Diana Perez <dicapegu95@gmail.com>
2023-11-23 18:47:44 -0500

    Ajuste style.css

--- database/conexion.js
+++ database/conexion.js
@@ -7,7 +7,7 @@ const mysql = require('mysql')
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '123456',
     database: 'hospital'
 })
 
